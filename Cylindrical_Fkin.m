a9022bc56fb2af9ab99a7402371b56cf707e1695
patrickverroya <21-05720@g.batstate-u.edu.ph>
2024-02-28 23:06:50 +0800

    Update Cylindrical_Fkin.m

--- Cylindrical_Fkin.m
+++ Cylindrical_Fkin.m
@@ -9,9 +9,16 @@ a3 = 50;
 %% Joint Variables\
 d2 = 20;
 d3 = 20;
+
 %% D-H Parameters [theta, d, r, alpha, offset]
 % if prismatic joint: theta = theta, d = 0, offset = 1, after offset put the value of d
 % if revolute joint: theta = 0,offset = 0, after offset put the value of theta
 H0_1 = Link([0,a1,0,0,0,0]);
 H0_1.qlim = [-pi/2 pi/2];
+
+H1_2 = Link([3*pi/2,0,0,3*pi/2,1,a2]);
+H1_2.qlim = [0 d2];
+
+H2_3 = Link([0,0,0,0,1,a3]);
+H2_3.qlim = [0 d3];
 
