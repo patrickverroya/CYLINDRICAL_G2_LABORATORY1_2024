d077a9e4e7ec1596871dcc06529dfbca468af3ee
patrickverroya <157588949+patrickverroya@users.noreply.github.com>
2024-02-29 01:31:21 +0800

    Update Ikincylindrical.py

--- Ikincylindrical.py
+++ Ikincylindrical.py
@@ -5,9 +5,9 @@
 import numpy as np
 
 #link lengths in mm
-a1=float(input("a1 ="))
-a2=float(input("a2 ="))
-a3=float(input("a3 ="))
+a1=float(input("a1 = "))
+a2=float(input("a2 = "))
+a3=float(input("a3 = "))
 
 
 #Position Vectors in mm
