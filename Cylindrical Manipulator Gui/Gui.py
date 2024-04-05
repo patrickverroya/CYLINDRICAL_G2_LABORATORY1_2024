ff1f2d37b8f92f806b68a05cd2c99b22b25c46cc
patrickverroya <157588949+patrickverroya@users.noreply.github.com>
2024-04-05 22:50:18 +0800

    Create Gui.py

---
+++ Cylindrical Manipulator Gui/Gui.py
@@ -0,0 +1,13 @@
+
+from tkinter import *
+from tkinter import messagebox
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
+import numpy as np
+import math
+import roboticstoolbox as rtb
+from roboticstoolbox import DHRobot, RevoluteDH, PrismaticDH
+import spatialmath
+from spatialmath import SE3
+import matplotlib
+matplotlib.use("TkAgg")
